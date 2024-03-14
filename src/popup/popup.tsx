59961c5a152e010b6fb17a819ac5c1301dac0d1a
Kareem <kareemsnn@gmail.com>
2024-03-14 18:11:35 -0400

    idk

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,3 +1,16 @@
 import React from 'react';
+import {createRoot} from 'react-dom/client'
 
-const test = <h1>Hello World</h1>
+
+const test = (
+    <div>
+    <h1>Hello World</h1>
+    <p>yooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</p>
+    </div>
+)
+
+
+const container = document.createElement('div')
+document.body.appendChild(container)
+const root = createRoot(container)
+root.render(test)
