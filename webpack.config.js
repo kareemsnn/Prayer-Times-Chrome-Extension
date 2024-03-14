baa75b2536e48161420d18ebdf42ccf924f8c22a
Kareem <kareemsnn@gmail.com>
2024-03-14 08:48:37 -0400

    idk

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: {
+        popup: path.resolve('./src/popup/popup.tsx')
+    },
+    module: {
+        rules: [
+            {
+                use:"ts-loader",
+                test: /\.tsx$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+plugins:[
+    new CopyPlugin({
+        patterns: [
+          { from: path.resolve('src/assets/manifest.json'), to: path.resolve('dist') },
+          { from: path.resolve('icons.png'), to: path.resolve('dist') },
+        ],
+    }),
+    new HtmlPlugin({
+        title: 'React Project',
+        filename:'popup.html',
+        chunks: ['popup']
+    })
+],
+resolve: {
+    extensions: ['.tsx','.ts','.js']
+},
+    output:{
+        filename: '[name].js'
+    }
+}
